4c7eb3a4204b034f18f3e30822e432d21cfbbdb0
Admin <maayush235@gmail.com>
2024-05-22 14:18:37 +0530

    Radhey Rad hey

--- lib/Providers/Chat_Screen_Provider.dart
+++ lib/Providers/Chat_Screen_Provider.dart
@@ -294,7 +294,7 @@ class ChatProvider extends ChangeNotifier {
                 onPressed: () {
                   Navigator.pop(context);
                 },
-                child: Text("Cancel"))
+                child: const Text("Cancel"))
           ],
         ),
       ),
